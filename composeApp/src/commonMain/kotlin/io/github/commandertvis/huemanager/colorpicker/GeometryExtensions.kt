1286aeba5480f4964868f017603baaf81704912d
Iaroslav Postovalov <postovalovya@gmail.com>
2026-01-19 23:24:51 +0100

    refactor: Remove unused geometry extensions

--- composeApp/src/commonMain/kotlin/io/github/commandertvis/huemanager/colorpicker/GeometryExtensions.kt
+++ composeApp/src/commonMain/kotlin/io/github/commandertvis/huemanager/colorpicker/GeometryExtensions.kt
@@ -16,7 +16,6 @@
 package io.github.commandertvis.huemanager.colorpicker
 
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import kotlin.math.PI
 import kotlin.math.atan2
@@ -34,22 +33,11 @@ internal fun Float.toDegrees() = this * TO_DEGREES
 
 // These exist for Size but not IntSize
 internal val IntSize.center get() = Offset(width * 0.5f, height * 0.5f)
-internal operator fun IntSize.times(scale: Float) =
-  IntSize((width * scale).roundToInt(), (height * scale).roundToInt())
-
-// New extensions
 internal val IntSize.radius get() = min(width, height) * 0.5f
 
 internal val Offset.minCoordinate get() = min(x, y) // exists for Size but not Offset
-internal fun Offset.distanceTo(other: Offset) = sqrt(
-  (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y),
-)
-
-internal fun Offset.midpoint(other: Offset) = (this + other) * 0.5f
 internal fun Offset.length() = sqrt(x * x + y * y)
 internal fun Offset.angle() = atan2(y, x)
-
-internal fun Offset.roundToInt() = IntOffset(x.roundToInt(), y.roundToInt())
 
 internal fun Offset.Companion.fromAngle(radians: Float, magnitude: Float) =
   Offset(cos(radians) * magnitude, sin(radians) * magnitude)
