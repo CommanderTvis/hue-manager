b5c51e8fc08794865e7e29bb3636fafcd11385bb
Iaroslav Postovalov <postovalovya@gmail.com>
2026-03-21 02:59:39 +0100

    Support dark window bar

--- composeApp/src/jvmMain/kotlin/io/github/commandertvis/huemanager/main.kt
+++ composeApp/src/jvmMain/kotlin/io/github/commandertvis/huemanager/main.kt
@@ -3,11 +3,16 @@ package io.github.commandertvis.huemanager
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "hue-manager",
-    ) {
-        App()
+fun main() {
+    // Match macOS title bar to system dark/light theme
+    System.setProperty("apple.awt.application.appearance", "system")
+
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "hue-manager",
+        ) {
+            App()
+        }
     }
 }
